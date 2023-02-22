2413ea005e2a4f5096243b253fe4af067e2235e7
Karl <karl.widkor@gmail.com>
2023-02-22 10:29:54 +0100

    final server with environment variables and instructions

---
+++ server/README.md
@@ -0,0 +1,19 @@
+## STEPS TO RUN SERVER
+
+1. Move to ~/react-cryptochat/server
+2. run 
+    ```bash 
+    npm install
+    ```
+4. Create an .env file. 
+5. Add 
+    ```bash
+   PORT=4000
+   ```
+1. Save the file and start server: 
+    ```bash
+    npm run dev
+    ```
+
+1. Server should now be running and console print: **"LISTENING ON PORT <port>"**
+   
--- server/server.js
+++ server/server.js
@@ -1,50 +1,59 @@
 "use strict";
 exports.__esModule = true;
-var cors = require("cors");
-var path = require('path');
-var bodyParser = require('body-parser');
 var express = require('express');
-var jsonParser = bodyParser.json();
+require('dotenv').config(); // how can we get rid of require?
+// Initilize express app and create the "database" that in our case is a simple array.
 var app = express();
-var PORT = 8000;
-/**
- * INPUT
- */
-var test_msg = {
-    msg: "Hello everyone!",
-    name: "Anonymous",
-    date: new Date()
-};
-app.use(express.static(path.resolve(__dirname, '../build')));
-//app.use(cors);
+var msg_array = new Array;
+// This makes it possible to pass json objects.
 app.use(express.json());
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
-var allowedOrigins = ['http://localhost:3000', 'http://localhost:8000'];
-var options = { origin: allowedOrigins };
-app.use(cors(options));
-//app.use(cors());
-// Gets all message.
-app.get('/getAllMessages', function (req, res) {
-    //res.json({msg: 'this is from /api. '});
-    res.json({ "msg": 1 });
-    //res.send('Get all messages');
+/**
+ * This is middleware that is called before every call to the backend.
+ * Here we simply log the route and request method in console for debugging purposes.
+ * This will not be visable on user end.
+ */
+app.use(function (req, res, next) {
+    console.log(req.path, req.method);
+    next();
 });
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-// post chat message to server.
-app.post('/postChat', urlencodedParser, function (req, res) {
-    res.json({ msg: 'this is from /api. ' });
-    //console.log(req.body);
-    //console.log(req)
-    //res.send("hello");
-    //res.send('Get all messages');
+// Routes
+/**
+ * When GET method is called on 'http://localhost:4000/api/messages'
+ * all previous sent messages by the POST request will be sent to the client.
+ * This response is a simple array that we are using as our databse.
+ */
+app.get('/api/messages', function (req, res) {
+    var all_messages = msg_array;
+    res.status(200).json(all_messages);
 });
 /**
- * FOR FUTURE:
- * DELETE MESSAGE
- * GET ONE MESSAGE?
+ * When the POST method is called on 'http://localhost:4000/api/messages'
+ * we first deconstruct the body of the request, we expect it to be a json,
+ * that contains a message and a username. The ID is needed for the react
+ * component so that is also created in getID function.
+ *
+ * We have this in a try catch, so if any errors occurs during the add to the
+ * database we send back code 400 with the error. If the add is success
+ * we simply return with code 200 (everything ok) and with data that is added to
+ * the databse.
  */
-app.listen(PORT, function () {
-    console.log('[SERVER]: Server is running on port:', PORT);
+app.post('/api/messages', function (req, res) {
+    var _a = req.body, messagestring = _a.messagestring, username = _a.username;
+    try {
+        var msg = { msg: messagestring, name: username, id: 1 };
+        msg_array.push(msg);
+        res.status(200).json(msg);
+    }
+    catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+// ONLY FOR TEST, WILL BE REMOVED BEFORE PRODUCTION
+app.get('/', function (req, res) {
+    res.json({ msg: 'welcome to our app' });
+});
+// Listen for requests, Default is port 4000 and defined in environment 
+// as an environment variable to 
+app.listen(process.env.PORT, function () {
+    console.log("Listening on port", process.env.PORT);
 });
