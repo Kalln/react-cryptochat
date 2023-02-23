50aa3f61782af2f307ccb97acbf3792acbfcf585
Kristoffer Kulju <kulju.kristoffer@hotmal.com>
2023-02-23 11:27:10 +0200

    eddelanderuta prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import { Popup } from "./components/Settings"
-import { Messager } from "./components/Message_window.js"
+import { Messages } from "./Messages.js"
 
 import Stars from "./Backgrounds/StarStruck.png"
 
@@ -16,20 +16,19 @@ function App() {
   const [Key, setKey] = useState("apa")
   
   return (
-    <div style={{ backgroundImage: `url(${Bg})` }}>
-
+    <div style={{ backgroundImage: `url(${Bg})`, height: '800px' }}>
       <button onClick={() => setPopup_flag(true)}>Settings</button>
 
         {Popup_flag ? <Popup Key={Key} setKey={setKey} Bg={Bg} setBg={setBg} setUsername={setUsername} text="Settings" 
         closePopup={() => setPopup_flag(false)}/> : null} 
-      
-      <h1>Hej {Username}</h1> 
-      
+
+      <h1 style={{textAlign: 'center'}}> Hi {Username}!</h1> 
       <main>
       Welcome to CryptoChat. Press the settings-button in the top-left corner if you wish to change your name, encryption key or the theme.
       CryptoChat encrypts the contets of your messages, but your name will still be visible to all. Happy chatting!
       </main>
-      {Messager(Key)}
+
+      {Messages(Key)}
     </div>
   );
 }
