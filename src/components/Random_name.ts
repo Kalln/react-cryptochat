20e35c2e8416f0397d3228b1a6dc00d5f604ff74
Karl <karl.widkor@gmail.com>
2023-03-06 16:07:40 +0100

    minor bug fix and remove lib folder

--- src/components/Random_name.ts
+++ src/components/Random_name.ts
@@ -6,7 +6,7 @@
  * @example Rand_name() might return "Kille Turing"
  **/
 
-function Rand_name(): string {
+export function Rand_name(): string {
     //Arrays containing lists of first and surnames respectively
     const firstname_list: Array<string> = ["Kristoffer", "Viktor", "Kalle", "George", "Donald", "Hillary", "Bill", "Angela", "Emanuel", "Sanna", "Uffe", "Magdalena", "Sauli", "Vladimir", "Winston"]
     const surname_list: Array<string> = ["Berners-Lee", "Turing", "Liskov", "Babbage", "Ritchie", "Knuth", "von Neumann", "Gore", "Thompson", "Euler", "Gauss", "Noether", "Lovelace", "Cauchy"]
