b458cb7ad242048d094fd5e5e2cee4a8c535ec54
Kristoffer Kulju <kulju.kristoffer@hotmal.com>
2023-03-06 16:52:48 +0200

    kommenterade ocg städade upp random name funnktionen

--- src/components/Random_name.ts
+++ src/components/Random_name.ts
@@ -1,11 +1,20 @@
-const förnamn_lista: Array<string> = ["Kristoffer", "Viktor", "Kalle", "George", "Donald", "Hillary", "Bill", "Angela", "Emanuel", "Sanna", "Uffe", "Magdalena", "Sauli", "Vladimir", "Winston"]
-const titel_lista: Array<string> = ["Berners-Lee", "Turing", "Liskov", "Babbage", "Ritchie", "Knuth", "von Neumann", "Gore", "Thompson", "Euler", "Gauss", "Noether", "Lovelace", "Cauchy"]
+/**
+ * Creates a "random" name by picking a first name and surname from t
+ *
+ * @returns  {string} -A randomly generated name
+ *
+ * @example Rand_name() might return "Kille Turing"
+ **/
 
-export function Rand_name(): string {
-    const förnamn  = förnamn_lista[Math.floor(Math.random() * förnamn_lista.length)];
-    const titel  = titel_lista[Math.floor(Math.random() * titel_lista.length)];
+function Rand_name(): string {
+    //Arrays containing lists of first and surnames respectively
+    const firstname_list: Array<string> = ["Kristoffer", "Viktor", "Kalle", "George", "Donald", "Hillary", "Bill", "Angela", "Emanuel", "Sanna", "Uffe", "Magdalena", "Sauli", "Vladimir", "Winston"]
+    const surname_list: Array<string> = ["Berners-Lee", "Turing", "Liskov", "Babbage", "Ritchie", "Knuth", "von Neumann", "Gore", "Thompson", "Euler", "Gauss", "Noether", "Lovelace", "Cauchy"]
 
-    return förnamn + " " + titel;
+    const firstname  = firstname_list[Math.floor(Math.random() * firstname_list.length)];
+    const surname  = surname_list[Math.floor(Math.random() * surname_list.length)];
+
+    return firstname + " " + surname;
 }
 
 export default Rand_name;
